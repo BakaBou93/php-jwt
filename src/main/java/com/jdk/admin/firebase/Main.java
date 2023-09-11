039bd0736079b8c64a9723952d81afd45db8577a
BakaBou93 <110843563+BakaBou93@users.noreply.github.com>
2023-09-11 22:31:57 +0700

    update demo

--- src/main/java/com/jdk/admin/firebase/Main.java
+++ src/main/java/com/jdk/admin/firebase/Main.java
@@ -1,19 +1,59 @@
 package com.jdk.admin.firebase;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        try {
+            FileInputStream serviceAccount = new FileInputStream("./push-notifications-3c140-f40e8cfb60d4.json");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+            GoogleCredentials googleCredentials = GoogleCredentials.fromStream(serviceAccount);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(googleCredentials)
+                .build();
+
+            // Initialize the default app
+            FirebaseApp firebaseApp = FirebaseApp.initializeApp(options);
+
+            // Retrieve services by passing the defaultApp variable...
+            FirebaseAuth defaultAuth = FirebaseAuth.getInstance(firebaseApp);
+
+            String token = defaultAuth.createCustomToken("test_uid");
+
+            System.out.println("Token :" + token);
+
+            // This registration token comes from the client FCM SDKs.
+            String registrationToken = "dxC0GrLQqa5agBNnqIOLXy:APA91bEUy7lMR8Qsh2s5IGAX9mPHqkEb0N8y9IWW2-vuIB_K8TQsRhJN95lCTdAWNVdzAqolwi_zMjVe6N7JaNsOmtKZl_SyjIh9yrvdF5aVoyR6DTxixWRKlMfl1jXgCDrgwTyWq9rz";
+
+            // See documentation on defining a message payload.
+            Message message = Message.builder()
+                    .putData("score", "850")
+                    .putData("time", "2:45")
+                    .setToken(registrationToken)
+                    .build();
+
+            // Send a message to the device corresponding to the provided
+            // registration token.
+            String response = FirebaseMessaging.getInstance().send(message);
+            // Response is a message ID string.
+            System.out.println("Successfully sent message: " + response);
+
+        } catch (FileNotFoundException | FirebaseAuthException | FirebaseMessagingException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IO: " + e.getMessage());
         }
     }
 }
